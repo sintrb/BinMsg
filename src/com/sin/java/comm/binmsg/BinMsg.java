b43d872f8cc2e7c55b6db5b640276fc07e60df48
RobinTang <sintrb@gmail.com>
2014-12-26 15:19:55 +0800

    修正

--- src/com/sin/java/comm/binmsg/BinMsg.java
+++ src/com/sin/java/comm/binmsg/BinMsg.java
@@ -1,3 +1,6 @@
+/**
+ * GNU GENERAL PUBLIC LICENSE V2
+ */
 package com.sin.java.comm.binmsg;
 
 import java.io.IOException;
@@ -15,9 +18,12 @@ import com.sin.java.comm.binmsg.stream.SeekableOutputStream;
  * 
  * @author RobinTang
  * @date 2014-12-26
+ * @version 1.0
  * @see https://github.com/sintrb/BinMsg/
  */
 final public class BinMsg {
+	public static final String VERSION = "1.0"; // 版本
+	public static final int VERSION_HEX = 0x10; // 版本
 
 	public static final Class<?> TYPE_BYTES = (new byte[0]).getClass();
 	public static final Class<?> TYPE_SHORTS = (new short[0]).getClass();
