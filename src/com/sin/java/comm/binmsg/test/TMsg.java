5c100878e4238b926beb85aae4cd6c0531d942a3
RobinTang <sintrb@gmail.com>
2014-12-28 23:40:13 +0800

    测试

--- src/com/sin/java/comm/binmsg/test/TMsg.java
+++ src/com/sin/java/comm/binmsg/test/TMsg.java
@@ -1,26 +1,27 @@
 package com.sin.java.comm.binmsg.test;
 
 import com.sin.java.comm.binmsg.MsgBody;
+import com.sin.java.comm.binmsg.MsgBody.LayoutType;
 import com.sin.java.comm.binmsg.MsgField;
 import com.sin.java.comm.binmsg.MsgField.FieldType;
 
-@MsgBody
+@MsgBody(type = LayoutType.Offset)
 public class TMsg {
 	@MsgField(offset = 0, type = FieldType.BYTE)
 	public byte len1;
 
-	@MsgField(offset = 2, type = FieldType.BYTES, sizedepend = "len1", sizeunit = 1)
+	@MsgField(offset = 2, type = FieldType.BYTES, sizedepend = "len1")
 	public byte[] data1;
 
 	@MsgField(offset = 10, type = FieldType.BIG16)
 	public short len2;
 
-	@MsgField(offset = 12, type = FieldType.BIG16S, sizedepend = "len2", sizeunit = 1)
+	@MsgField(offset = 12, type = FieldType.BIG16S, sizedepend = "len2")
 	public short[] data2;
 
 	@MsgField(offset = 20, type = FieldType.LIT16)
 	public int len3;
 
-	@MsgField(offset = 22, type = FieldType.LIT16S, sizedepend = "len3", sizeunit = 1)
+	@MsgField(offset = 22, type = FieldType.LIT16S, sizedepend = "len3")
 	public int[] data3;
 }
